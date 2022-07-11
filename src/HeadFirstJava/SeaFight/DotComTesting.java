61cf4c945d09545a78383d95f339899e6120dab9
Ivsmcrew <Ivsmcrew@gmail.com>
2022-07-11 21:29:22 +0300

    Cheeeeeesssseeee

--- src/HeadFirstJava/SeaFight/DotComTesting.java
+++ src/HeadFirstJava/SeaFight/DotComTesting.java
@@ -1,7 +1,18 @@
 package HeadFirstJava.SeaFight;
 
+import java.util.ArrayList;
+
 public class DotComTesting {
     public static void main(String[] args) {
-
+        DotCom test = new DotCom();
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("a3");
+        list.add("b3");
+        list.add("c3");
+        test.setLocationCells(list);
+        //Проверка метода "checkYourself" класса DotCom
+        System.out.println(list.get(1));
+        System.out.println(test.checkYourself("b3"));
+        System.out.println(list.get(1));
     }
 }
